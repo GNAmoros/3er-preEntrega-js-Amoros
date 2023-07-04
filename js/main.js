a15ab0663bba8cd3d9b81a06507260c0a2618801
Nicolas Amoros <nicolas.amoros@gmail.com>
2023-07-04 16:10:31 -0300

    creating modals, next adding info

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ toggleBtn.addEventListener('click', () => {
 })
 
 const services = [
-    { id: 1, name: "Acido hialuronico", cost: 45000, area: "Estetica"},
-    { id: 2, name: "Dermapen", cost: 7500, area: "Estetica"},
-    { id: 3, name: "Fosfatidilcolina", cost: 8000, area: "Estetica"},
-    { id: 4, name: "Peeling quimico", cost: 4000, area: "Estetica"},
+    { id: 1, name: "Acido hialuronico", cost: 45000, area: "Estetica", img:"acidohialuronico.webp"},
+    { id: 2, name: "Dermapen", cost: 7500, area: "Estetica", img:"dermapen.webp"},
+    { id: 3, name: "Fosfatidilcolina", cost: 8000, area: "Estetica", img:"fosfatidilcolina.webp"},
+    { id: 4, name: "Peeling quimico", cost: 4000, area: "Estetica", img:"peelingquimico.webp"},
     { id: 5, name: "Pap/Colpo", cost: 1000, area: "Ginecologia"},
     { id: 6, name: "Test de HPV", cost: 1200, area: "Ginecologia" },
     { id: 7, name: "Consejeria en salud sexual y reproductiva", cost: 3000, area: "Ginecologia"},
@@ -72,12 +72,93 @@ const services = [
 // ]
 
 
-let container = document.getElementById("productAes");
+let aesthetic = document.getElementById("productAes");
 
 services.forEach(service => {
     if (service.area === "Estetica") {
     let aesCard = document.createElement("div")
-    aesCard.innerText = service.name
-    container.appendChild(aesCard)
+    aesCard.className = "productCard"
+    aesCard.innerHTML = `
+        <h3>${service.name}<h3>
+        <img class="aesImg" src="../img/${service.img}">
+        <div class="aesImg" style="background-image: url(../img/${service.img}"></div>
+        `;
+    aesCard.addEventListener("click", () => {
+        showModal(service);
+    });
+    aesthetic.appendChild(aesCard);
+}
+})
+function showModal(service) {
+    const modal = document.createElement("div");
+    modal.className = "modal";
+    modal.setAttribute("tabindex", "-1");
+
+    const modalDialog = document.createElement("div");
+    modalDialog.className = "modal-dialog";
+
+    const modalContent = document.createElement("div");
+    modalContent.className = "modal-content";
+
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+
+    const modalTitle = document.createElement("h5");
+    modalTitle.className = "modal-title";
+    modalTitle.textContent = service.name;
+
+    const closeButton = document.createElement("button");
+    closeButton.type = "button";
+    closeButton.className = "btn-close";
+    closeButton.setAttribute("data-bs-dismiss", "modal");
+    closeButton.setAttribute("aria-label", "Close");
+
+    modalHeader.appendChild(modalTitle);
+    modalHeader.appendChild(closeButton);
+
+    const modalBody = document.createElement("div");
+    modalBody.className = "modal-body";
+    modalBody.innerHTML = `
+        <p>Service ID: ${service.id}</p>
+        <p>Cost: ${service.cost}</p>
+        <p>Area: ${service.area}</p>
+    `;
+
+    const modalFooter = document.createElement("div");
+    modalFooter.className = "modal-footer";
+
+    const closeButtonFooter = document.createElement("button");
+    closeButtonFooter.type = "button";
+    closeButtonFooter.className = "btn btn-secondary";
+    closeButtonFooter.setAttribute("data-bs-dismiss", "modal");
+    closeButtonFooter.textContent = "Close";
+
+    modalFooter.appendChild(closeButtonFooter);
+
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+    modalContent.appendChild(modalFooter);
+
+    modalDialog.appendChild(modalContent);
+
+    modal.appendChild(modalDialog);
+
+    document.body.appendChild(modal);
+
+    const bootstrapModal = new bootstrap.Modal(modal);
+    bootstrapModal.show();
+}
+
+let gineco = document.getElementById("productGineco");
+
+services.forEach(service => {
+    if (service.area === "Ginecologia") {
+    let ginecoCard = document.createElement("div")
+    ginecoCard.className = "productCard"
+    ginecoCard.innerHTML = `
+        <h3>${service.name}<h3>
+        <div class="aesImg" style="background-image: url(../img/${service.img}"></div>
+        `
+    gineco.appendChild(aesCard)
 }
 })
