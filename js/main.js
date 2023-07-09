2f60eacaa2c63ac34aad301367d789f83ceee9b4
Nicolas Amoros <nicolas.amoros@gmail.com>
2023-07-09 17:08:48 -0300

    working on dates, time, name

--- js/main.js
+++ js/main.js
@@ -18,104 +18,225 @@ const services = [
     { id: 10, name: "Rehabilitacion del suelo pelvico", cost: 6000, area: "Obstetricia" },
     { id: 11, name: "Post-parto", cost: 7500, area: "Obstetricia" },
     { id: 12, name: "Curso pre-parto", cost: 5000, area: "Obstetricia" }
-];
-
-let aesthetic = document.getElementById("productAes");
-
-services.forEach(service => {
+  ];
+  
+  let aesthetic = document.getElementById("productAes");
+  
+  services.forEach(service => {
     if (service.area === "Estetica") {
-        let aesCard = document.createElement("div");
-        aesCard.className = "productCard";
-        aesCard.innerHTML = `
-            <h3>${service.name}<h3>
-            <img class="aesImg" src="../img/${service.img}">
-        `;
-        aesCard.addEventListener("click", () => {
-            showModal(service);
-        });
-        aesthetic.appendChild(aesCard);
+      let aesCard = document.createElement("div");
+      aesCard.className = "productCard";
+      aesCard.innerHTML = `
+        <h3>${service.name}</h3>
+        <img class="aesImg" src="../img/${service.img}">
+      `;
+      aesCard.addEventListener("click", () => {
+        showModal(service);
+      });
+      aesthetic.appendChild(aesCard);
     }
-});
-
-function showModal(service) {
+  });
+  
+  function showModal(service) {
     const modal = document.createElement("div");
     modal.className = "modal";
     modal.setAttribute("tabindex", "-1");
-
+  
     const modalDialog = document.createElement("div");
     modalDialog.className = "modal-dialog";
-
+  
     const modalContent = document.createElement("div");
     modalContent.className = "modal-content";
-
+  
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
-
+  
     const modalTitle = document.createElement("h5");
     modalTitle.className = "modal-title";
     modalTitle.textContent = service.name;
-
+  
     const closeButton = document.createElement("button");
     closeButton.type = "button";
     closeButton.className = "btn-close";
     closeButton.setAttribute("data-bs-dismiss", "modal");
     closeButton.setAttribute("aria-label", "Close");
-
+  
     modalHeader.appendChild(modalTitle);
     modalHeader.appendChild(closeButton);
-
+  
     const modalBody = document.createElement("div");
     modalBody.className = "modal-body";
     modalBody.innerHTML = `
-        <p>${service.description}</p>
-        <p>Costo: ${service.cost}</p>
-        <p>Area: ${service.area}</p>
+      <p>${service.description}</p>
+      <p>Costo: ${service.cost}</p>
+      <p>Area: ${service.area}</p>
     `;
-
+  
     const modalFooter = document.createElement("div");
     modalFooter.className = "modal-footer";
-
+  
     const closeButtonFooter = document.createElement("button");
     closeButtonFooter.type = "button";
     closeButtonFooter.className = "btn btn-secondary";
     closeButtonFooter.setAttribute("data-bs-dismiss", "modal");
     closeButtonFooter.textContent = "Close";
-
+  
     modalFooter.appendChild(closeButtonFooter);
-
+  
     modalContent.appendChild(modalHeader);
     modalContent.appendChild(modalBody);
     modalContent.appendChild(modalFooter);
-
+  
     modalDialog.appendChild(modalContent);
-
+  
     modal.appendChild(modalDialog);
-
+  
     document.body.appendChild(modal);
-
+  
     const bootstrapModal = new bootstrap.Modal(modal);
     bootstrapModal.show();
+  }
+  
+  console.log('desde index.js');
+  
+  let appointmentData = JSON.parse(localStorage.getItem("appointments")) || [];
+  
+  const saveAppointment = (name, date, time) => {
+    const startTime = new Date(date + "T09:00:00");
+    const endTime = new Date(date + "T18:00:00");
+  
+    const selectedTime = new Date(date + "T" + time);
+    
+    if (selectedTime >= startTime && selectedTime <= endTime) {
+      appointmentData.push({ name, date, time });
+      localStorage.setItem("appointments", JSON.stringify(appointmentData));
+      console.log(appointmentData);
+    } else {
+      alert("El horario elegido se encuentra fuera del horario de trabajo. Por favor elija un horario entre las 9:00 y las 18:00.");
+    }
+  };
+  
+  const saveBtn = document.getElementById("saveDate");
+  
+  saveBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const nameInput = document.getElementById("name");
+    const dateInput = document.getElementById("date");
+    const timeSelect = document.getElementById("time");
+  
+    const name = nameInput.value.trim();
+    const date = dateInput.value;
+    const time = timeSelect.value;
+  
+    if (name !== "" && date !== "" && time !== "") {
+      saveAppointment(name, date, time);
+      nameInput.value = "";
+      dateInput.value = "";
+      timeSelect.value = "";
+    }
+  });
+
+const availableTimes = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
+
+function populateTimeDropdown() {
+  const timeSelect = document.getElementById("time");
+
+  timeSelect.innerHTML = "";
+
+  availableTimes.forEach((time) => {
+    const option = document.createElement("option");
+    option.value = time;
+    option.textContent = time;
+    timeSelect.appendChild(option);
+  });
 }
 
-console.log('desde index.js');
+populateTimeDropdown();
 
-const dateArray = JSON.parse(localStorage.getItem('dates')) || [];
-
-const btn = document.getElementById('saveDate');
-
-const saveAppointment = () => {
-    const dateInput = document.getElementById('date');
-    const date = dateInput.value;
-
-    if (date.trim() !== '') {
-        dateArray.push(date);
-        localStorage.setItem('dates', JSON.stringify(dateArray));
-        console.log(dateArray);
-        dateInput.value = '';
-    }
-};
-
-btn.addEventListener('click', (e) => {
+  
+  const searchBtn = document.getElementById("searchDate");
+  
+  searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    saveAppointment();
-})
+    const searchInput = document.getElementById("searchName");
+    const searchTerm = searchInput.value.trim();
+  
+    if (searchTerm !== "") {
+      const filteredAppointments = appointmentData.filter(
+        (appointment) =>
+          appointment.name.toLowerCase() === searchTerm.toLowerCase()
+      );
+      console.log(filteredAppointments);
+    }
+  });
+  
+  const displayModalBtn = document.getElementById("searchDate");
+  
+  displayModalBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const searchInput = document.getElementById("searchName");
+    const searchTerm = searchInput.value.trim();
+  
+    if (searchTerm !== "") {
+      const filteredAppointments = appointmentData.filter(
+        (appointment) =>
+          appointment.name.toLowerCase() === searchTerm.toLowerCase()
+      );
+      displaySavedAppointments(filteredAppointments);
+    }
+  });
+  
+  function displaySavedAppointments(appointments) {
+    const modal = document.createElement("div");
+    modal.className = "modal";
+    modal.setAttribute("tabindex", "-1");
+  
+    const modalDialog = document.createElement("div");
+    modalDialog.className = "modal-dialog";
+  
+    const modalContent = document.createElement("div");
+    modalContent.className = "modal-content";
+  
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+  
+    const modalTitle = document.createElement("h5");
+    modalTitle.className = "modal-title";
+    modalTitle.textContent = "Saved Appointments";
+  
+    const closeButton = document.createElement("button");
+    closeButton.type = "button";
+    closeButton.className = "btn-close";
+    closeButton.setAttribute("data-bs-dismiss", "modal");
+    closeButton.setAttribute("aria-label", "Close");
+  
+    modalHeader.appendChild(modalTitle);
+    modalHeader.appendChild(closeButton);
+  
+    const modalBody = document.createElement("div");
+    modalBody.className = "modal-body";
+  
+    if (appointments.length === 0) {
+      modalBody.textContent = "No se encontraron reservas";
+    } else {
+      const ul = document.createElement("ul");
+      appointments.forEach((appointment) => {
+        const li = document.createElement("li");
+        li.textContent = `Name: ${appointment.name}, Date: ${appointment.date}, Time: ${appointment.time}`;
+        ul.appendChild(li);
+      });
+      modalBody.appendChild(ul);
+    }
+  
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+  
+    modalDialog.appendChild(modalContent);
+  
+    modal.appendChild(modalDialog);
+  
+    document.body.appendChild(modal);
+  
+    const bootstrapModal = new bootstrap.Modal(modal);
+    bootstrapModal.show();
+  }
